64328fde7850cb25628a5a41bfdbb1d025d2428e
Mehmet Teker <teker.mehmed@gmail.com>
2021-05-02 17:51:54 +0300

    first commit

---
+++ SherlockAndTheValidString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace SherlockAndTheValidString
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        { 
+            Console.WriteLine();
+        }
+    }
+}
