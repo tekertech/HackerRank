0fcb2eb3410f05ac41d99a87917ddd73afc4c0ec
Mehmet Teker <teker.mehmed@gmail.com>
2021-05-02 17:54:06 +0300

    Sherlock and the Valid String

--- SherlockAndTheValidString/Program.cs
+++ SherlockAndTheValidString/Program.cs
@@ -15,10 +15,59 @@ using System;
 namespace SherlockAndTheValidString
 {
     class Program
-    { 
+    {
+        static string isValid(string s)
+        {
+            List<char> characters = new List<char>();
+            foreach (var item in s)
+            {
+                characters.Add(item);
+            }
+
+            var groupByCharacter = from character in characters
+                                   group character by character into result
+                                   select new
+                                   {
+                                       Character = result.Key,
+                                       Count = result.Count()
+                                   };
+            int maxCount = groupByCharacter.Max(x => x.Count);
+            int minCount = groupByCharacter.Min(x => x.Count);
+
+
+            if (maxCount - minCount == 0)
+            {
+                return "YES";
+            }
+            else
+            {
+                int x = groupByCharacter.Where(a => a.Count == maxCount).Count();
+                int y = groupByCharacter.Where(a => a.Count == minCount).Count();
+
+                var newCharacterList = groupByCharacter.GroupBy(b => b.Count);
+
+                if ((maxCount - minCount == 1) && (x == 1 || y == 1))
+                {
+                    return "YES";
+                }
+                else if ((newCharacterList.Count() == 2) && ((x == 1 && maxCount == 1) || (y == 1 && minCount == 1)))
+                {
+                    return "YES";
+                }
+                else
+                {
+                    return "NO";
+                }
+            }
+        }
+
         static void Main(string[] args)
-        { 
-            Console.WriteLine();
+        {
+            string s = "aaaabbcc"; // "ibfdgaeadiaefgbhbdghhhbgdfgeiccbiehhfcggchgghadhdhagfbahhddgghbdehidbibaeaagaeeigffcebfbaieggabcfbiiedcabfihchdfabifahcbhagccbdfifhghcadfiadeeaheeddddiecaicbgigccageicehfdhdgafaddhffadigfhhcaedcedecafeacbdacgfgfeeibgaiffdehigebhhehiaahfidibccdcdagifgaihacihadecgifihbebffebdfbchbgigeccahgihbcbcaggebaaafgfedbfgagfediddghdgbgehhhifhgcedechahidcbchebheihaadbbbiaiccededchdagfhccfdefigfibifabeiaccghcegfbcghaefifbachebaacbhbfgfddeceababbacgffbagidebeadfihaefefegbghgddbbgddeehgfbhafbccidebgehifafgbghafacgfdccgifdcbbbidfifhdaibgigebigaedeaaiadegfefbhacgddhchgcbgcaeaieiegiffchbgbebgbehbbfcebciiagacaiechdigbgbghefcahgbhfibhedaeeiffebdiabcifgccdefabccdghehfibfiifdaicfedagahhdcbhbicdgibgcedieihcichadgchgbdcdagaihebbabhibcihicadgadfcihdheefbhffiageddhgahaidfdhhdbgciiaciegchiiebfbcbhaeagccfhbfhaddagnfieihghfbaggiffbbfbecgaiiidccdceadbbdfgigibgcgchafccdchgifdeieicbaididhfcfdedbhaadedfageigfdehgcdaecaebebebfcieaecfagfdieaefdiedbcadchabhebgehiidfcgahcdhcdhgchhiiheffiifeegcfdgbdeffhgeghdfhbfbifgidcafbfcd";// Console.ReadLine();
+            s = "ibfdgaeadiaefgbhbdghhhbgdfgeiccbiehhfcggchgghadhdhagfbahhddgghbdehidbibaeaagaeeigffcebfbaieggabcfbiiedcabfihchdfabifahcbhagccbdfifhghcadfiadeeaheeddddiecaicbgigccageicehfdhdgafaddhffadigfhhcaedcedecafeacbdacgfgfeeibgaiffdehigebhhehiaahfidibccdcdagifgaihacihadecgifihbebffebdfbchbgigeccahgihbcbcaggebaaafgfedbfgagfediddghdgbgehhhifhgcedechahidcbchebheihaadbbbiaiccededchdagfhccfdefigfibifabeiaccghcegfbcghaefifbachebaacbhbfgfddeceababbacgffbagidebeadfihaefefegbghgddbbgddeehgfbhafbccidebgehifafgbghafacgfdccgifdcbbbidfifhdaibgigebigaedeaaiadegfefbhacgddhchgcbgcaeaieiegiffchbgbebgbehbbfcebciiagacaiechdigbgbghefcahgbhfibhedaeeiffebdiabcifgccdefabccdghehfibfiifdaicfedagahhdcbhbicdgibgcedieihcichadgchgbdcdagaihebbabhibcihicadgadfcihdheefbhffiageddhgahaidfdhhdbgciiaciegchiiebfbcbhaeagccfhbfhaddagnfieihghfbaggiffbbfbecgaiiidccdceadbbdfgigibgcgchafccdchgifdeieicbaididhfcfdedbhaadedfageigfdehgcdaecaebebebfcieaecfagfdieaefdiedbcadchabhebgehiidfcgahcdhcdhgchhiiheffiifeegcfdgbdeffhgeghdfhbfbifgidcafbfcd";
+            string result = isValid(s);
+
+            Console.WriteLine(result);
         }
     }
 }
